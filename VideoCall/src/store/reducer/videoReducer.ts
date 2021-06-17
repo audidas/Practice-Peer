2d44270c80e3a81d34793ccf83d6fa76f3688588
audidas <audidas@naver.com>
2021-06-17 18:15:58 +0900

    react native 추가:

---
+++ VideoCall/src/store/reducer/videoReducer.ts
@@ -0,0 +1,8 @@
+const initialState = {};
+
+export default (state = initialState, {type, payload}: any) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const morgan = require("morgan");
+
+const app = express();
+const server = http.createServer(app);
+
+
+const {ExpressPeerServer} = require('peer');
+
+const io = socketio(server).sockets;
+
+app.use(express.json());
+
+
+const customGenerationFunction =() =>{
+    return (Math.random().toString(36) + "0000000000000000000").substr(2,16);
+}
+
+const peerServer = ExpressPeerServer(server,{
+    debug:true,
+    path:'/',
+    generateClientId:customGenerationFunction,
+});
+
+app.use("/mypeer",peerServer);
+
+io.on('connection', (socket)=>{
+    console.log('connected');
+})
+
+const port = process.env.PORT || 5000;
+
+server.listen(port,()=>console.log(`Server is running on port ${port}`));
